de33640b29315e49fc50c29591c40e3c866f4283
hllwrld <hllwrld1006@gmail.com>
2025-12-27 16:17:43 +0300

    -

--- app/src/main/java/com/example/ur_color/ui/theme/AppScaffold.kt
+++ app/src/main/java/com/example/ur_color/ui/theme/AppScaffold.kt
@@ -1,29 +1,14 @@
 package com.example.ur_color.ui.theme
 
 import android.content.pm.ActivityInfo
-import androidx.compose.foundation.ScrollState
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.WindowInsetsSides
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.only
-import androidx.compose.foundation.layout.safeDrawing
-import androidx.compose.foundation.layout.windowInsetsBottomHeight
-import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.SnackbarHost
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalView
-import androidx.core.view.WindowCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.ur_color.ui.screen.LocalBottomBarState
 import com.example.ur_color.utils.findActivity
 

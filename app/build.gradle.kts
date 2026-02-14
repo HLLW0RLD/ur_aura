718da330059b1f9af836fea0a1e81063dec1bc92
hllwrld <hllwrld1006@gmail.com>
2026-02-14 15:20:12 +0300

    - поднята версия

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.example.ur_color"
         minSdk = 25
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.2"
+        versionCode = 3
+        versionName = "1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }

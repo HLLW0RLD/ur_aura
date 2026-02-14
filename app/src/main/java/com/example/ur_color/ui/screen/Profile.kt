3aea79897b85a3eefbca82e887a854bff749ec67
hllwrld <hllwrld1006@gmail.com>
2026-02-14 19:32:17 +0300

    - ддогбавлен скрывающийся тулбар на экран профиля

--- app/src/main/java/com/example/ur_color/ui/screen/Profile.kt
+++ app/src/main/java/com/example/ur_color/ui/screen/Profile.kt
@@ -2,7 +2,12 @@ package com.example.ur_color.ui.screen
 
 import android.os.Build
 import androidx.annotation.RequiresApi
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.Image
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -13,6 +18,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
@@ -20,13 +26,18 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.input.nestedscroll.NestedScrollConnection
+import androidx.compose.ui.input.nestedscroll.NestedScrollSource
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.Velocity
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
@@ -66,6 +77,7 @@ fun Profile(profile : Profile) {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @RequiresApi(Build.VERSION_CODES.VANILLA_ICE_CREAM)
 @Composable
 fun ProfileScreen(
@@ -82,25 +94,222 @@ fun ProfileScreen(
     var showBottomSheet by remember { mutableStateOf(false) }
     val color = AppColors.backgroundDark
 
+    val scrollState = rememberLazyListState()
+    val scrollThreshold = 24f
+    var accumulatedScroll by remember { mutableFloatStateOf(0f) }
+
+    var extended by remember { mutableStateOf(true) }
+
+    val nestedScrollConnection = remember {
+        object : NestedScrollConnection {
+            override fun onPreScroll(available: Offset, source: NestedScrollSource): Offset {
+                if (source != NestedScrollSource.Drag) return Offset.Zero
+                accumulatedScroll += available.y
+
+                when {
+                    accumulatedScroll > scrollThreshold &&
+                            !extended -> {
+                        extended = true
+                        accumulatedScroll = 0f
+                    }
+
+                    accumulatedScroll < -scrollThreshold &&
+                            extended -> {
+                        extended = false
+                        accumulatedScroll = 0f
+                    }
+                }
+
+                return Offset.Zero
+            }
+
+            override suspend fun onPostFling(
+                consumed: Velocity,
+                available: Velocity
+            ): Velocity {
+                accumulatedScroll = 0f
+                return Velocity.Zero
+            }
+        }
+    }
 
     Column(
         modifier = modifier
+            .nestedScroll(nestedScrollConnection)
             .fillMaxSize()
             .background(AppColors.background)
     ) {
         user?.let { u ->
-            CustomAppBar(
-                title = stringResource(R.string.profile_title),
-                showOptions = true,
-                optionsIcon = painterResource(R.drawable.switcher_options),
-                onOptionsClick = {
-                    navController.nav(Settings)
-                },
-                isCentered = true,
-                backgroundColor = AppColors.background,
-            )
+            Column(
+                modifier = modifier
+                    .statusBarsPadding()
+            ) {
+                AnimatedVisibility(
+                    visible = !extended,
+                ) {
+                    Row(
+                        modifier = Modifier
+                            .statusBarsPadding()
+                            .fillMaxWidth()
+                            .padding(horizontal = 8.dp),
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    ) {
+                        val avatarPainter = if (u.avatarUri != null) {
+                            u.avatarUri
+                        } else {
+                            "https://picsum.photos/seed/abstract02/600/600"
+                        }
+
+                        Row(
+                            modifier = Modifier,
+                            verticalAlignment = Alignment.CenterVertically,
+                            horizontalArrangement = Arrangement.SpaceBetween
+                        ) {
+                            AsyncImage(
+                                model = avatarPainter,
+                                contentDescription = "",
+                                contentScale = ContentScale.Crop,
+                                modifier = Modifier
+                                    .size(48.dp)
+                                    .clip(CircleShape)
+                            )
+                            Spacer(Modifier.width(8.dp))
+                            Text(
+                                color = AppColors.textPrimary,
+                                text = u.nickName,
+                                textAlign = TextAlign.Center,
+                                style = MaterialTheme.typography.titleMedium
+                            )
+                        }
+
+                        IconButton(
+                            onClick = {
+                                navController.nav(Settings)
+                            }
+                        ) {
+                            Icon(
+                                painter = painterResource(R.drawable.switcher_options),
+                                contentDescription = "",
+                                tint = AppColors.icon
+                            )
+                        }
+                    }
+                }
+
+                AnimatedVisibility(
+                    visible = extended
+                ) {
+                    Column {
+                        CustomAppBar(
+                            title = stringResource(R.string.profile_title),
+                            showOptions = true,
+                            optionsIcon = painterResource(R.drawable.switcher_options),
+                            onOptionsClick = {
+                                navController.nav(Settings)
+                            },
+                            isCentered = true,
+                            showDivider = true,
+                            backgroundColor = AppColors.background,
+                        )
+
+                        Spacer(Modifier.height(16.dp))
+
+                        Row(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(horizontal = 16.dp),
+                            verticalAlignment = Alignment.Top,
+                            horizontalArrangement = Arrangement.SpaceBetween
+                        ) {
+                            val avatarPainter = if (u.avatarUri != null) {
+                                u.avatarUri
+                            } else {
+                                "https://picsum.photos/seed/abstract02/600/600"
+                            }
+
+                            Row(
+                                modifier = Modifier
+                            ) {
+                                AsyncImage(
+                                    model = avatarPainter,
+                                    contentDescription = "",
+                                    contentScale = ContentScale.Crop,
+                                    modifier = Modifier
+                                        .size(130.dp)
+                                        .clip(CircleShape)
+                                )
+
+                                Spacer(Modifier.width(8.dp))
+
+                                Column {
+                                    Text(
+                                        color = AppColors.textPrimary,
+                                        text = stringResource(
+                                            R.string.profile_name_level,
+                                            u.nickName,
+                                            u.userLevel
+                                        ),
+                                        style = MaterialTheme.typography.titleMedium
+                                    )
+                                    Text(
+                                        color = AppColors.textPrimary,
+                                        text = stringResource(
+                                            R.string.profile_zodiac,
+                                            u.zodiacSign.lowercase()
+                                        ),
+                                        style = MaterialTheme.typography.bodyMedium
+                                    )
+
+                                    var expanded by remember { mutableStateOf(false) }
+                                    var isOverflowing by remember { mutableStateOf(false) }
+                                    Column(
+                                        modifier = Modifier
+                                            .animateContentSize()
+                                    ) {
+                                        Text(
+                                            text = u.about.orEmpty(),
+                                            color = AppColors.textPrimary,
+                                            style = MaterialTheme.typography.bodyMedium,
+                                            maxLines = if (expanded) Int.MAX_VALUE else 5,
+                                            overflow = TextOverflow.Ellipsis,
+                                            onTextLayout = { textLayoutResult ->
+                                                isOverflowing = textLayoutResult.hasVisualOverflow
+                                            }
+                                        )
+
+                                        if (isOverflowing || expanded) {
+                                            Spacer(Modifier.height(4.dp))
+
+                                            Text(
+                                                text = if (expanded) "скрыть" else "ещё",
+                                                style = MaterialTheme.typography.labelMedium,
+                                                color = AppColors.accentPrimary,
+                                                modifier = Modifier
+                                                    .fillMaxWidth()
+                                                    .clickable(
+                                                        indication = null,
+                                                        interactionSource = remember { MutableInteractionSource() }
+                                                    ) {
+                                                        expanded = !expanded
+                                                    }
+                                            )
+                                        }
+                                    }
+
+                                    // dropdown with full user info
+                                    // ${calculateAge(u.birthDate)}
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            Spacer(Modifier.height(16.dp))
 
             LazyColumn(
+                state = scrollState,
                 modifier = Modifier
                     .fillMaxWidth(),
                 verticalArrangement = Arrangement.spacedBy(12.dp),
@@ -112,92 +321,6 @@ fun ProfileScreen(
                             .background(color.copy(alpha = 0.2f))
                             .padding(vertical = 16.dp)
                     ) {
-                        Row(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .padding(horizontal = 16.dp),
-                            verticalAlignment = Alignment.Top
-                        ) {
-                            val avatarPainter = if (u.avatarUri != null) {
-                                u.avatarUri
-                            } else {
-                                "https://picsum.photos/seed/abstract02/600/600"
-                            }
-
-                            AsyncImage(
-                                model = avatarPainter,
-                                contentDescription = "",
-                                contentScale = ContentScale.Crop,
-                                modifier = Modifier
-                                    .size(130.dp)
-                                    .clip(CircleShape)
-                            )
-
-                            Spacer(Modifier.width(8.dp))
-
-                            Column {
-                                Text(
-                                    color = AppColors.textPrimary,
-                                    text = stringResource(
-                                        R.string.profile_name_level,
-                                        u.firstName,
-                                        u.userLevel
-                                    ),
-                                    style = MaterialTheme.typography.titleMedium
-                                )
-                                Text(
-                                    color = AppColors.textPrimary,
-                                    text = stringResource(
-                                        R.string.profile_zodiac,
-                                        u.zodiacSign.lowercase()
-                                    ),
-                                    style = MaterialTheme.typography.bodyMedium
-                                )
-
-                                var expanded by remember { mutableStateOf(false) }
-                                var isOverflowing by remember { mutableStateOf(false) }
-                                Column(
-                                    modifier = Modifier
-                                        .fillMaxWidth()
-                                        .animateContentSize()
-                                ) {
-                                    Text(
-                                        text = u.about.orEmpty(),
-                                        color = AppColors.textPrimary,
-                                        style = MaterialTheme.typography.bodyMedium,
-                                        maxLines = if (expanded) Int.MAX_VALUE else 5,
-                                        overflow = TextOverflow.Ellipsis,
-                                        onTextLayout = { textLayoutResult ->
-                                            isOverflowing = textLayoutResult.hasVisualOverflow
-                                        }
-                                    )
-
-                                    if (isOverflowing || expanded) {
-                                        Spacer(Modifier.height(4.dp))
-
-                                        Text(
-                                            text = if (expanded) "скрыть" else "ещё",
-                                            style = MaterialTheme.typography.labelMedium,
-                                            color = AppColors.accentPrimary,
-                                            modifier = Modifier
-                                                .fillMaxWidth()
-                                                .clickable(
-                                                    indication = null,
-                                                    interactionSource = remember { MutableInteractionSource() }
-                                                ) {
-                                                    expanded = !expanded
-                                                }
-                                        )
-                                    }
-                                }
-
-                                // dropdown with full user info
-                                // ${calculateAge(u.birthDate)}
-                            }
-                        }
-
-                        Spacer(Modifier.height(16.dp))
-
                         Text(
                             color = AppColors.textAuto(AppColors.surfaceLight),
                             text = stringResource(R.string.profile_aura_achievement),
